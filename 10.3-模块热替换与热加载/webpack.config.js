4ac5b7876fe51e190376402e9be8ea31f0212c5a
BarryDong <286084845@qq.com>
2022-04-27 10:08:43 +0800

    模块热替换与热加载

---
+++ 10.3-模块热替换与热加载/webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './app.js',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        })
+    ],
+    devServer: {
+        // hot: true,
+
+        hot: false,
+        liveReload: false
+    }
+}
